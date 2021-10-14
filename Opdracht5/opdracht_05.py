ba3f6f5b5cc597d2725d4a9569854fbdd0c2dfe6
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-10-14 17:19:51 +0200

    opdr 5

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,7 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+def favorite_cars(name): 
+    print("one of my favorite cars is", name)
+
+favorite_cars("Mercedes")
+favorite_cars("Audi")
+favorite_cars("Bmw")    
