5a8c5258bda37e335e3dad2c3526531cc2b13383
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-10-30 10:06:22 +0200

    eindopdracht.updated

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -1,8 +1,19 @@
+print ("Welcome!")
+print ("pleas answer in English and click space once before you answer")  
 
-def favorite_cities(name): 
-    print("one of my favorite cities is", name)
+def een():
 
-favorite_cities("New York")
-favorite_cities("Amsterdam")
-favorite_cities("Berlin")    
+    a = input ("what is your name?")
+    print("hello, " + a )
 
+    b = input ("are you older then 12?") 
+    if b == 'no' or b == 'NO':
+     print ("We are sorry, but you have to be above 12")  
+    if b == 'yes' or b == 'YES':
+     print ("thats good to hear!")
+
+    c = input ("How old are you then?") 
+    print("thats good to hear, " + a ) 
+
+een()
+ 
